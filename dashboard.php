573544a3a2d0dc6da44fa3372c73c31cb9b376b0
Md. Al-Amin <41773444+alamin22@users.noreply.github.com>
2020-07-29 20:11:38 -0700

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -72,16 +72,46 @@
 			<div class="col-12 col-sm-12 col-md-5 col-lg-5 mt-3">
 				<div class="card">
 					<div class="col-12 col-sm-12 col-md-12 col-lg-12">
-						<h3>Coming Soon.......</h3>
-						<?php
-							echo rand();
-						?>
+						<div class="row">
+						    <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
+							 <h1 class="mb-3 mt-4 text-danger text-center" id="dashboard_clock"></h1>
+						    </div>
+						    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 nopadding">
+							 <h2 class="mb-1 mt-4 text-danger" style="font-size: 22px" id="meridian"></h2>
+						    </div>
+						</div>
+						<div class="row">
+							<h5 class="ml-4">Date: <?php echo date('d-m-Y')?></h5>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+<script type="text/javascript">
+	 function updateClock ()
+	    {
+	    var currentTime = new Date ();
+	    var currentHours = currentTime.getHours ();
+	    var currentMinutes = currentTime.getMinutes ();
+	    var currentSeconds = currentTime.getSeconds ();
+	    currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+	    currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+	    var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+	    currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+	    currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+	    var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds;
 
+	    $("#dashboard_clock").html(currentTimeString);
+	    $("#meridian").html(timeOfDay);
+
+	 }
+
+	$(document).ready(function()
+	{
+	   setInterval('updateClock()', 1000);
+	});
+</script>
 <?php
 	include"footer.php";
 ?>
